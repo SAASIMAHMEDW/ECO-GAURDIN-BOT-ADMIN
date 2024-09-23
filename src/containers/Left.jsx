feadc55e617abf37f8acfb45a518693074678a59
“Ahmed” <aasimahmedsiddiqui1@gmail.com>
2024-09-24 00:15:50 +0530

    bug: file error

--- src/containers/Left.jsx
+++ src/containers/Left.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import "./left.css";
+import "./Left.css";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import BotCard from "@/components/ui/BotCard";
 import {Bots} from "../assets/Data";
