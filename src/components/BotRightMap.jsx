d41460d736e43691610754836986ade3ea443190
S. AASIM'AHMED W. <aasimahmedsiddiqui1@gmail.com>
2024-09-25 00:59:50 +0530

    fix: leaflet css added

--- src/components/BotRightMap.jsx
+++ src/components/BotRightMap.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { Marker, Popup } from "react-leaflet";
-
+import "leaflet/dist/leaflet.css";
 function BotRightMap() {
   return (
     <>
