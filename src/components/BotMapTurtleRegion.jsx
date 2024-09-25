03fccd7afbf2c38532a47b46ed837200760d6978
S. AASIM'AHMED W. <aasimahmedsiddiqui1@gmail.com>
2024-09-26 01:21:32 +0530

    changed: map tiles

--- src/components/BotMapTurtleRegion.jsx
+++ src/components/BotMapTurtleRegion.jsx
@@ -13,9 +13,10 @@ function BotMapTurtleRegion() {
         scrollWheelZoom={false}
       >
          <TileLayer
-          attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={`https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}?api_key=${import.meta.env.VITE_STADIA_MAP_API_KEY}`}
+        attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          // url={`https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}?api_key=${import.meta.env.VITE_STADIA_MAP_API_KEY}`}
           // url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           ext="png"
         />
         {TurtlesRegion.map((marker) => (
