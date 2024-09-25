d917ce4f02ac2f4c47e9a187d8c03ecf4c428925
S. AASIM'AHMED W. <aasimahmedsiddiqui1@gmail.com>
2024-09-25 17:52:41 +0530

    add: added sattelite map for all

--- src/components/BotMapTurtleRegion.jsx
+++ src/components/BotMapTurtleRegion.jsx
@@ -12,9 +12,11 @@ function BotMapTurtleRegion() {
         zoom={20}
         scrollWheelZoom={false}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+         <TileLayer
+          attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}?api_key=${import.meta.env.VITE_STADIA_MAP_API_KEY}`}
+          // url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}"
+          ext="jpg"
         />
         {TurtlesRegion.map((marker) => (
           <CircleMarker
