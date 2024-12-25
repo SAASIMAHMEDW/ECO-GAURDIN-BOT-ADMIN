0878372f8933456956520022be7b308dddc001a1
SUMANTHBEKAL <sumanthbekal505@gmail.com>
2024-12-26 04:38:12 +0530

    theme adjusted

--- src/components/magics/NoData.jsx
+++ src/components/magics/NoData.jsx
@@ -3,13 +3,13 @@ import { MessageCircleWarning } from "lucide-react";
 
 const NoData = ({ title, desc, date }) => {
   return (
-    <div className="h-[calc(100vh-75px)] w-full max-w-lg rounded-lg bg-black p-6 text-white">
+    <div className="h-[calc(100vh-75px)] w-full max-w-lg rounded-lg bg-white dark:bg-black p-6 text-black dark:text-white">
       <div className="mt-4">
-        <MessageCircleWarning className="text-green-400" />
-        <p className="text-green-400">{title}</p>
-        <p className="text-white">{desc}</p>
-        <p className="text-white">Time</p>
-        <p className="text-green-400">{date}</p>
+        <MessageCircleWarning className="text-red-400 dark:text-green-400" />
+        <p className="text-red-400 dark:text-green-400">{title}</p>
+        <p className="text-black dark:text-white">{desc}</p>
+        <p className="text-black dark:text-white">Time</p>
+        <p className="text-red-400 dark:text-green-400">{date}</p>
       </div>
     </div>
   );
